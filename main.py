6c29a149575392826263283c9825e2dbfbb0b263
vitaly.gusev <vitaly.gusev@progforce.com>
2022-03-23 16:21:46 +0300

    interim code 0.2 - predict original model

--- main.py
+++ main.py
@@ -14,16 +14,23 @@ import logging
 import numpy as np
 import PIL.Image as Image
 
-
-
-
-
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
+def format_image(image, label):
+  image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES))/255.0
+  return image, label
 
-
-
+def plot_dogsAndcats(image_batch, predicted_class_names):
+    fig = plt.figure(figsize=(10, 9))
+    for n in range(30):
+        plt.subplot(6, 5, n + 1)
+        plt.subplots_adjust(hspace=0.3)
+        plt.imshow(image_batch[n])
+        plt.title(predicted_class_names[n])
+        plt.axis('off')
+        _ = plt.suptitle("ImageNet predictions")
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -69,9 +76,39 @@ if __name__ == '__main__':
         _ = plt.title("Prediction: " + predicted_class_name.title())
         plt.show()
 
+#=====================================================================================================>
 
+    #  Download Cats vs. Dogs dataset
+    (train_examples, validation_examples), info = tfds.load(
+        'cats_vs_dogs',
+        with_info=True,
+        as_supervised=True,
+        split=['train[:80%]', 'train[80%:]'],
+    )
 
+    num_examples = info.splits['train'].num_examples
+    num_classes = info.features['label'].num_classes
 
+    # Check size of images in dataset
+    for i, example_image in enumerate(train_examples.take(5)):
+        print("Image {} shape: {}".format(i+1, example_image[0].shape))
+
+    # Oooops... they are all different sizes
+    BATCH_SIZE = 32
+    train_batches = train_examples.shuffle(num_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+    validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+    # ImageNet has a lot of dogs and cats in it, so let's see if original "mobilenet_v2" can predict the images in our Dogs vs. Cats dataset
+    image_batch, label_batch = next(iter(train_batches.take(1)))
+    image_batch = image_batch.numpy()
+    label_batch = label_batch.numpy()
+
+    result_batch = model.predict(image_batch)
+    predicted_class_names = imagenet_labels[np.argmax(result_batch, axis=-1)]
+
+    plot_dogsAndcats(image_batch, predicted_class_names)
+
+#======================== transfer learning with TensorFlow Hub =======================================>
 
 
 
